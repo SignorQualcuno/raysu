e7661cd2b7626bd815c104aa9ac12512201474c4
Enrico <enricomoras06@gmail.com>
2026-08-08 13:09:40 +0200

    feat(game): add ability to quit game by pressing the Q key

--- src/scenes/menu_scene.cpp
+++ src/scenes/menu_scene.cpp
@@ -16,11 +16,12 @@ MenuScene::MenuScene(std::vector<Button> buttons, std::vector<Label> labels) {
 void MenuScene::init(Engine *engine) {
     std::cout << "MENU_SCENE_INIT" << std::endl;
     m_buttons.push_back(
-        {Rectangle{0.0f, 0.0f, 100, 100}, RED, Label{"Play"},
+        {Rectangle{0, 0, 100, 100}, RED, Label{"Play"},
          [engine]() { engine->setScene(std::make_unique<GameScene>()); }});
     m_buttons.push_back(
-        {Rectangle{150.0f, 0.0f, 100, 100}, GREEN, Label{"Settings"},
+        {Rectangle{150.0f, 0, 100, 100}, GREEN, Label{"Settings"},
          [engine]() { engine->setScene(std::make_unique<SettingsScene>()); }});
+    m_staticLabels.push_back(Label{"Press Q to quit", {0, 100}});
     m_staticLabels.push_back(
         Label{"Scene: Menu", {2, (float)GetScreenHeight() - 20}});
 }
@@ -35,6 +36,9 @@ void MenuScene::render() {
 }
 
 void MenuScene::update(float deltaTime, Engine *engine) {
+    if (IsKeyPressed(KEY_Q)) {
+        CloseWindow();
+    }
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
         for (Button &button : m_buttons) {
             if (button.collisionDetected(GetMousePosition())) {
